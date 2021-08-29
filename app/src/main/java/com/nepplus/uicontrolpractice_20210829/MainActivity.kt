e8c6e34779b5bd6bca4d631e0c991881b9cc64b2
김다은 <softeun92@naver.com>
2021-08-29 12:38:06 +0900

    set 기능 체험용 UI 작업 (set)

--- app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
@@ -23,5 +23,22 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
 
         }
+
+        //  카피메세지버튼이 눌리면 - > 메세지가 적힌 내용을 받아와서 = > 텍스트뷰의 문구(text)로 반영
+
+
+        copyMessageBtn.setOnClickListener {
+
+            //입력한 내용 받아오기. (변수에 저장)
+            val inputmessage = messageEdt.text.toString()
+
+            //메세지 텍스트뷰의 text 속성에, 내용을 대입.   (set)
+
+            messageTxt.text = inputmessage
+
+
+        }
+
+
     }
 }
