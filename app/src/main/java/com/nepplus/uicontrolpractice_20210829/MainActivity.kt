a18758e0da7bd91c8fddb5bb5ac3709d7099a3eb
김다은 <softeun92@naver.com>
2021-08-29 12:25:54 +0900

    inputessage toast

--- app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
@@ -2,10 +2,26 @@ package com.nepplus.uicontrolpractice_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //  qjxmsdl snfflaus => aptpwlrk wjrgls sodyddmf qkedkdhktj => xhtmxmfmf Emldnsmsep tkdyd.
+
+        toastBtn.setOnClickListener {
+
+            //aptpwlrk dlqfurehls sodyddmf qkedkdhwk. => rldjrgoenjdi => qustnfmf aksemfdjtj wjwkdgoenwk
+            val inputMessage = messageEdt.text.toString()
+
+
+            //xhtmxmfmf Emldnfeo = > qustndp ekarls sodyddmf Emldnwk.
+
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
